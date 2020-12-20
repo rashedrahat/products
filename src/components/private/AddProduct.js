bee93041e666f6c3b1b45fb5e573af8ddca34fae
Rashed Rahat <imrashedrahat@gmail.com>
2020-12-20 16:16:08 +0600

    worked on list of products & adding a product

--- src/components/private/AddProduct.js
+++ src/components/private/AddProduct.js
@@ -1,21 +1,135 @@
 import React from 'react';
 import Navbar from "../layout/Navbar";
+import axios from "axios";
+import $ from "jquery";
+import { withSnackbar } from 'notistack';
 
 class AddProduct extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            data_to_post: {
+                title: '',
+                description: '',
+                price: '',
+            },
+        };
+
+        this.inputChangeHandler = this.inputChangeHandler.bind(this);
+        this.formSubmitHandler = this.formSubmitHandler.bind(this);
+        this.fileInput = React.createRef();
     }
+
+    handleSuccess(msg) {
+        this.key = this.props.enqueueSnackbar(msg, {
+            variant: 'success',
+            autoHideDuration: 2000,
+        });
+    }
+
+    handleError(msg) {
+        this.key = this.props.enqueueSnackbar(msg, {
+            variant: 'error',
+            autoHideDuration: 2000,
+        });
+    }
+
+    formSubmitHandler = (e) => {
+        e.preventDefault();
+        // console.log(this.state.data_to_post);
+        const headers = {
+            'Content-Type': 'multipart/form-data',
+        };
+
+        let form_data = new FormData();
+        form_data.append('title', this.state.data_to_post.title);
+        form_data.append('description', this.state.data_to_post.description);
+        form_data.append('price', this.state.data_to_post.price);
+        form_data.append('image', this.fileInput.current.files[0]);
+
+        const token = localStorage.getItem("userAuthToken");
+
+        axios.post(process.env.REACT_APP_API_ENDPOINT + 'products?token=' + token, form_data, {headers: headers}).then(response => {
+            console.log(response);
+            if (response.status === 200 && response.data.success) {
+                this.props.history.push('/products');
+                this.handleSuccess(response.data.message);
+            }
+        }).catch(error => {
+            if (error.response) {
+                console.log(error.response)
+                switch (error.response.status) {
+                    case 400:
+                        // console.log(error.response.data.error)
+                        const errors = error.response.data.error;
+                        for (const property in errors) {
+                            // console.log(`${property}: ${errors[property]}`);
+                            $('#' + property + "-val-err-msg").text(errors[property])
+                        }
+                        break;
+                    default:
+                        this.handleError('Something went wrong');
+                }
+            }
+        });
+    };
+
+    inputChangeHandler = (e) => {
+        $('#' + e.target.name + "-val-err-msg").text('')
+        let obj = this.state.data_to_post;
+        obj[e.target.name] = e.target.value;
+        this.setState({data_to_post: obj});
+    };
+
     render() {
         return (
             <div>
                 <Navbar/>
                 <div className="container">
-
+                    <div className="w-75 mx-auto shadow p-5">
+                        <h2 className="text-center mb-4">Add a Product</h2>
+                        <form onSubmit={this.formSubmitHandler}>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control form-control-lg"
+                                    placeholder="Enter Title"
+                                    name="title" value={this.state.data_to_post.title}
+                                    onChange={this.inputChangeHandler} required
+                                />
+                                <small className="text-danger" id="title-val-err-msg"></small>
+                            </div>
+                            <div className="form-group">
+                                <textarea
+                                    className="form-control form-control-lg"
+                                    placeholder="Write something about this product..."
+                                    name="description" onChange={this.inputChangeHandler}
+                                    required>{this.state.data_to_post.description}</textarea>
+                                <small className="text-danger" id="description-val-err-msg"></small>
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="number" min={1}
+                                    className="form-control form-control-lg"
+                                    placeholder="Enter Price" value={this.state.data_to_post.price}
+                                    name="price" onChange={this.inputChangeHandler} required
+                                />
+                                <small className="text-danger" id="price-val-err-msg"></small>
+                            </div>
+                            <div className="form-group">
+                                <div className="custom-file">
+                                    <input type="file" name="image" className="form-control form-control-lg custom-file-input" ref={this.fileInput} required/>
+                                    <label className="custom-file-label">Choose file</label>
+                                </div>
+                                <small className="text-danger" id="image-val-err-msg"></small>
+                            </div>
+                            <button className="btn btn-primary btn-block" type='submit'>Add</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         );
     }
 }
 
-export default AddProduct
+export default withSnackbar(AddProduct)
--- src/components/private/Products.js
+++ src/components/private/Products.js
@@ -1,17 +1,109 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import Navbar from "../layout/Navbar";
+import axios from "axios";
 
 class Products extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            products: []
+        };
     }
+
+    componentDidMount() {
+        const token = localStorage.getItem("userAuthToken");
+        axios.get(process.env.REACT_APP_API_ENDPOINT + 'products/?token=' + token).then(response => {
+            console.log(response);
+            if (response.status === 200) {
+                // console.log(response.data)
+                this.setState({products: response.data})
+            }
+        }).catch(error => {
+            if (error.response) {
+                console.log(error.response)
+                const errorMsg = error.response.data.error;
+                switch (error.response.status) {
+                    case 400:
+                        if (errorMsg === 'token_not_provided')
+                            this.props.history.push('/login')
+                        break;
+                    case 401:
+                        if (errorMsg === 'token_expired')
+                            this.props.history.push('/login')
+                        break;
+                    default:
+                        this.handleError('Something went wrong');
+                }
+            }
+        });
+    }
+
+    deleteProduct = (id) => {
+        console.log(id)
+    }
+
     render() {
+        const {products} = this.state;
         return (
             <div>
                 <Navbar/>
                 <div className="container">
+                    <div className="py-4">
+                        <h1>List of Products</h1>
+                        <table className="table border shadow">
+                            <thead className="thead-light">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Image</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {products.length > 0 &&
+                            <>
+                                {
+                                    products.map((product, index) => (
+                                        <tr key={index}>
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{product.title}</td>
+                                            <td>${product.price}</td>
+                                            <td>
+                                                <img
+                                                    src={process.env.REACT_APP_IMAGE_ENDPOINT || 'http://127.0.0.1:8000/storage/images/' ?
+                                                        process.env.REACT_APP_IMAGE_ENDPOINT + product.image_name
+                                                        : 'http://127.0.0.1:8000/storage/images/' + product.image_name}
+                                                    className="rounded-circle" alt={product.image_name}
+                                                    style={{width: '50px', height: '50px'}}/>
+                                            </td>
+                                            <td>
+                                                <Link className="btn btn-primary mr-2" to={`/products/${product.id}`}>
+                                                    View
+                                                </Link>
+                                                <Link
+                                                    className="btn btn-outline-primary mr-2"
+                                                    to={`/products/edit/${product.id}`}
+                                                >
+                                                    Edit
+                                                </Link>
+                                                <button className="btn btn-danger"
+                                                        onClick={() => this.deleteProduct(product.id)}>Delete
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </>
+                            }
 
+                            {products.length === 0 && <tr>
+                                <td colSpan={5}>No products found.</td>
+                            </tr>}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         );
